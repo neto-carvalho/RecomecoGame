2dc5575731ccaf73bff7c424a4fa95ba64f748d2
Windrose-git <almir.junior445@gmail.com>
2026-03-15 14:21:19 -0300

    Inclusão de uma cena teste direto na pasta assets, contendo um mapa de design teste.

---
+++ Assets/fpscontroller.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float speed = 6f;
+    public float mouseSensitivity = 200f;
+
+    public Transform playerCamera;
+
+    float xRotation = 0f;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Movimento
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
